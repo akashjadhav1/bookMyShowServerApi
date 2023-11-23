447ca3eed87b4efa4902695e6862326708041ebf
Akash Jadhav <akashjadhav5975@gmail.com>
2023-11-23 17:35:14 +0530

    for mongo environment file add

--- createDatabase.js
+++ createDatabase.js
@@ -2,14 +2,24 @@ const MongoClient = require('mongodb').MongoClient;
 
 async function connectToDatabase() {
   try {
-    const client = await MongoClient.connect(process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/bookMyShow', {
+    const dbUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookMyShow';
+
+    if (!dbUrl.startsWith('mongodb://') && !dbUrl.startsWith('mongodb+srv://')) {
+      throw new Error('Invalid MongoDB URI scheme. It should start with "mongodb://" or "mongodb+srv://".');
+    }
+
+    const client = await MongoClient.connect(dbUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
+    console.log('Connected to MongoDB');
+
+    // Optionally, you can return the MongoDB client object if needed
     return client;
   } catch (error) {
     console.error('Error connecting to the database:', error);
-    throw error; // Rethrow the error to propagate it to the caller
+    throw error;
   }
 }
 
